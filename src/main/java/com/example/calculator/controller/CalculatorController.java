b68ece99c88c5b5dfcc2ffb645d7575ea033c887
Nisam Ponery <nisamponery@gmail.com>
2018-10-22 21:35:31 -0400

    added controler class with simple rest service

--- src/main/java/com/example/calculator/controller/CalculatorController.java
+++ src/main/java/com/example/calculator/controller/CalculatorController.java
@@ -1,5 +1,18 @@
 package com.example.calculator.controller;
 
-public class CalculatorController {
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/app")
+public class CalculatorController
+{
+ 
+	@GetMapping("/welcome")
+	public String welcome(){
+		return "Application under construction";
+		
+	}
 
 }
--- src/main/java/com/example/calculator/services/SubtractionService.java
+++ src/main/java/com/example/calculator/services/SubtractionService.java
@@ -4,5 +4,5 @@ public class SubtractionService {
 	
 	//TODO Create two int variables
 	//TODO Create Constructor to take the arguments 
-	//TODO Create method to return the sum of the 2 varaibles
+	//TODO Create method to return the difference of the 2 varaibles
 }
